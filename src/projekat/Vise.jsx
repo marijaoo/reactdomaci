ec9921093cc232ded94b418844639f4ac2ef2dd7
marijaoo <ocelmarija@gmail.com>
2023-06-07 19:41:51 +0200

    Rutiranje, navigacioni meni, futer

---
+++ src/projekat/Vise.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class Vise extends Component {
+  render() {
+    return (
+      <div>Vise</div>
+    )
+  }
+}
+
+export default Vise
--- src/projekat/components/Button.jsx
+++ src/projekat/components/Button.jsx
@@ -1,9 +1,16 @@
 import React, { Component } from 'react'
-
+import { Link } from 'react-router-dom'
 export default function Button ({text, key, onClick}) {
 
     return (
-      <button className={`btn `} key={key} onClick={onClick}> {text} </button>
+      <button className={`btn `} key={key} onClick={onClick}>
+        
+         <Link to="/vise" style={{ textDecoration: 'none', color: 'whitesmoke'}}>{text}</Link>
+
+          
+         
+         
+    </button>
     )
 
 }
